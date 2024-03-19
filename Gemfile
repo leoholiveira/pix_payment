2c0a1eb09fa40f7f078d432ef7c41745f57b59d2
Leonardo <leonardo.oliveira.nascimento@gmail.com>
2024-03-19 08:11:38 +0000

    initial commit

---
+++ Gemfile
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in pix_payment.gemspec
+gemspec
+
+gem "rake", "~> 13.0"
---
+++ lib/pix_payment.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "pix_payment/version"
+
+module PixPayment
+  class Error < StandardError; end
+  # Your code goes here...
+end
